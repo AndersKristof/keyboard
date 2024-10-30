379b0e96d47ed42095b004cd47b5877fd34e8b26
Anders Kristoffersen <Anders.Kristoffersen@microchip.com>
2024-10-30 16:25:18 +0100

    chore: initial commit

---
+++ firmware-du/firmware-du/main.c
@@ -0,0 +1,18 @@
+/*
+ * firmware-du.c
+ *
+ * Created: 30.10.2024 16:24:12
+ * Author : M78000
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
